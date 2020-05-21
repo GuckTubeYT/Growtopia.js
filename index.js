6e3d904ae75df8ee5baf71f49519dd5d2f1b6cce
GuckTube YT <56192597+GuckTubeYT@users.noreply.github.com>
2020-05-21 18:19:13 +0700

    Auto webserver

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const cp = require('child_process');
 module.exports = {
 	Main: require('./src/Main.js'),
 	Host: require('./src/Host.js'),
@@ -6,3 +7,7 @@ module.exports = {
 	Dialog: require('./src/Dialog'),
 	Constants: require('./src/structs/Constants')
 }
+    cp.exec('node web.js', function(error, out, input) {
+      if (error) 
+        throw new Error(error);
+	});
